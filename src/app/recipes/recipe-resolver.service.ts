346f61ca6c6533ea9954591b28050749b5639428
Val <valeriu.viscreanu@gmail.com>
2022-12-12 09:14:36 +0200

    Addded fetch recipes and resolver

---
+++ src/app/recipes/recipe-resolver.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { ignoreElements } from "rxjs-compat/operator/ignoreElements";
+import { DataStorageService } from "../shared/data-storage.service";
+import { Recipe } from "./recipe.models";
+import { RecipeService } from "./recipe.service";
+
+@Injectable({providedIn: 'root'})
+export class RecipeResolverService implements Resolve<Recipe[]>{
+    constructor( private readonly dataStorageService: DataStorageService, private recipesService: RecipeService){
+    }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+    {
+        const localRecipes = this.recipesService.getRecipes();
+        
+        if (localRecipes.length === 0)
+            return this.dataStorageService.getRecipes();
+        else
+            return localRecipes;
+    }
+}
