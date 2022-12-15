d8539fd22c6e72d56f27c8801efdf6eb0f922577
Val <valeriu.viscreanu@gmail.com>
2022-12-15 12:41:20 +0200

    Moved APi key to environment

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { BehaviorSubject, throwError } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
+import { environment } from "src/environments/environment";
 import { User } from "./user.model";
 ;
 
@@ -57,7 +58,7 @@ export class AuthService {
      }
 
     signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAziXB8m062L3gGA_iguB67n2ueFzrfDVo',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.fbApiKey,
             {
                 email: email,
                 password: password,
@@ -79,7 +80,7 @@ export class AuthService {
     }
 
     login(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAziXB8m062L3gGA_iguB67n2ueFzrfDVo',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.fbApiKey,
             {
                 email: email,
                 password: password,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fbApiKey: 'AIzaSyAziXB8m062L3gGA_iguB67n2ueFzrfDVo'
 };
 
 /*
